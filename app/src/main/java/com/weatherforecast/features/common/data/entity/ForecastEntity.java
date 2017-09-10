753f477f3af9e2e0bb84122617420b139b810a46
vinciusmoleta <vinicius.moleta@mttnow.com>
2017-09-10 12:25:26 +0100

    Updating gradle versions and room warning

--- app/src/main/java/com/weatherforecast/features/common/data/entity/ForecastEntity.java
+++ app/src/main/java/com/weatherforecast/features/common/data/entity/ForecastEntity.java
@@ -2,10 +2,12 @@ package com.weatherforecast.features.common.data.entity;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Index;
 
 @Entity(tableName = "forecast",
         primaryKeys = {"date", "cityId"},
-        foreignKeys = @ForeignKey(entity = CityEntity.class, parentColumns = "id", childColumns = "cityId")
+        foreignKeys = @ForeignKey(entity = CityEntity.class, parentColumns = "id", childColumns = "cityId"),
+        indices = @Index("cityId")
 )
 public class ForecastEntity {
 
--- app/src/main/java/com/weatherforecast/features/common/data/repository/ForecastDao.java
+++ app/src/main/java/com/weatherforecast/features/common/data/repository/ForecastDao.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.RoomWarnings;
 
 import com.weatherforecast.features.common.data.entity.ForecastEntity;
 
@@ -17,6 +18,7 @@ public interface ForecastDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(final List<ForecastEntity> forecasts);
 
+    @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
     @Query("SELECT * FROM forecast INNER JOIN city ON city.id = forecast.cityId WHERE city.id = :cityId")
     Flowable<List<ForecastEntity>> findForecastForCity(final long cityId);
 
