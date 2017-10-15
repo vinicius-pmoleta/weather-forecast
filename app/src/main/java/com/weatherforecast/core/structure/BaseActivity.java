a7344067a1d1565cf459b266157ea6526d6d7d38
vinciusmoleta <vinicius.moleta@mttnow.com>
2017-10-15 14:02:01 +0100

    Jacoco configuratio

--- app/src/main/java/com/weatherforecast/core/structure/BaseActivity.java
+++ app/src/main/java/com/weatherforecast/core/structure/BaseActivity.java
@@ -2,10 +2,11 @@ package com.weatherforecast.core.structure;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import javax.inject.Inject;
 
-public abstract class BaseActivity<Presenter extends BaseContract.Action> extends AppCompatLifecycleActivity {
+public abstract class BaseActivity<Presenter extends BaseContract.Action> extends AppCompatActivity {
 
     @Inject
     protected Presenter presenter;
